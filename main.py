d05c2a1266ec2533598b777c5435401695de3c61
laxman2021 <laxman2021@gmail.com>
2023-05-08 20:21:15 +0530

    initial commit

---
+++ main.py
@@ -0,0 +1,8 @@
+user_prompt = "Enter a ToDo:"
+todo1 = input(user_prompt)
+todo2 = input(user_prompt)
+todo3 = input(user_prompt)
+todo4 = input(user_prompt)
+
+todos = [todo1, todo2, todo3, todo4]
+print(todos)
